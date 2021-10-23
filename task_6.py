d1d400145436ea2b04a765e07029c20680d8c20f
Volya <volya.shai@gmail.com>
2021-10-23 14:41:36 +0300

    redone task_6

--- task_6.py
+++ task_6.py
@@ -1,15 +1,10 @@
-proceeds = float( input("Введите количество выручки:") )
-costs = float( input("Введите количество издержек:") )
-result = proceeds - costs
-if result > 0:
-    print(f"Фирма работает с прибылью {result:.2f}")
-    rent = result / proceeds * 100
-    print ( f"Рентабельность составляет {rent:.2f}")
-    numb_emp = int(input("Введите количество сотрудников:"))
-    profit_emp = rent / numb_emp
-    print( f"Выручка на одного сотрудника составляет: {profit_emp:.2f}")
-elif result < 0 :
-   print("Фирма работает с убылью")
-
+actually = float(input ("Сколько км спортсмен пробежал в первый день?"))
+purpose = float(input ("Не менее скольки км спортсмен хочет пробегать за 1 тренировку?"))
+days = 1
+if actually <= 0 or purpose < 0:
+    print ("Результат в первый день должен быть больше нуля, а желаемый больше или равен 0 ")
 else:
-    print("Прибыль равно 0, как и затраты")
+    while actually < purpose:
+        actually = actually * 1.1
+        days = days + 1
+print (f"На {days} день спортсмен достигнет своего результата")
