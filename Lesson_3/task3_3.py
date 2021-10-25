4c271ba5cf66b6a562df4b3065ba15b7a46a102f
Volya <volya.shai@gmail.com>
2021-10-25 18:12:54 +0300

    redone_2 task3_3

--- Lesson_3/task3_3.py
+++ Lesson_3/task3_3.py
@@ -1,13 +1,11 @@
 def my_func(numb_1, numb_2, numb_3):
-    numbers = [numb_1, numb_2, numb_3]
     try:
         # noinspection PyArgumentList
-        int(numb_1, numb_2, numb_3)
+        numb_1, numb_2, numb_3 = int(numb_1), int(numb_2), int(numb_3)
+        numbers = [numb_1, numb_2, numb_3]
         numbers.remove(min(numbers))
         return sum(numbers)
     except (ValueError, TypeError):
         print("Вводите числа!")
-
-
 print(my_func(input("Введите первое число: ",), input("Введите второе число: "),
         input("Введите третье число: ")))
